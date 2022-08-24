9c32e693a824d6c7b2f6f24ef6d28b74e2417851
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2022-08-24 15:56:20 +0600

    file read and write

--- app.js
+++ app.js
@@ -18,7 +18,24 @@ const server = http.createServer( (req, res) => {
             res.write(data);
             res.end();
         })
-    }else{
+    }else if(req.url == "/createfile"){
+        res.writeHead(200, {'Content-TYpe': 'text/html'})
+        const data = "<h1>New File</h1>";
+        fs.appendFile("temp/html.html", data, (err) => {
+            if(err) throw err;
+            res.write("file created");
+        })
+        res.end();
+    }else if(req.url == "/readfile"){
+        res.writeHead(200, { 'Content-Type' : 'text/html'});
+        fs.readFile("temp/html.html", "utf8", (err, data) => {
+            if(err) throw err;
+            res.write(data);
+            res.end();
+        })
+
+    }
+    else{
         res.writeHead(404, { 'Content-Type': 'text/html' });
         res.write("<h1>Page Not Found</h1>");
         res.end();        
