93f36a7ccedd510d97256f291274ff5ff3fb4736
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2022-08-24 15:37:25 +0600

    file system

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const http = require('http')
+const fs = require('fs');
 
 const server = http.createServer( (req, res) => {
     if(req.url == '/'){
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({
-          data: 'Hello World!'
-        }));
-    }else if(req.url == '/login'){
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({
-          data: 'Hello Login!'
-        }));
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        
+        fs.readFile("pages/home.html", "utf8", (err, data) => {
+            if(err) throw err;
+            res.write(data);
+            res.end();
+        })
+        
+    }else if(req.url == '/about'){
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.readFile("pages/about.html", "utf8", (err, data) => {
+            if(err) throw err;
+            res.write(data);
+            res.end();
+        })
     }else{
         res.writeHead(404, { 'Content-Type': 'text/html' });
         res.write("<h1>Page Not Found</h1>");
