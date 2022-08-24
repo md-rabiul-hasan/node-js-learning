b9124aa3aacb75517015639eae0c8f7b13cdee2d
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2022-08-24 12:10:16 +0600

    http server create

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+const http = require("http");
+
+const server = http.createServer( (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+        data: 'Hello World!'
+    }));
+} );
+
+server.listen(3000);
