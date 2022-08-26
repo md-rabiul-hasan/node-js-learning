eb96ccf66009e1f09d065a9ab41820f2e81bb10a
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2022-08-26 09:12:32 +0600

    event emitter

--- express-js/app.js
+++ express-js/app.js
@@ -1,8 +1,17 @@
+const EventEmitter = require('events');
+const myEmitter = new EventEmitter();
+
+myEmitter.on('event', (data) => {
+    console.log(data);
+});
+
+
 const express = require("express")
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
+    myEmitter.emit('event', {name: 'Rabiul Hasan'})
     res.send("Hello World")
 });
 
